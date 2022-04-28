df2741dca98b21ba508dcee2c416ad0b433a8ba2
BRIETGAME <alexis.briet2003@gmail.com>
2022-04-28 22:33:11 +0200

    New migrations

---
+++ migrations/Version20220428203115.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220428203115 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE game CHANGE game_time_next game_time_next TIME DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE game CHANGE game_time_next game_time_next VARCHAR(255) DEFAULT NULL');
+    }
+}
