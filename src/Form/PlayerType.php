4487cdeb23d85e5bcca02735abef40a47720f615
BRIETGAME <alexis.briet2003@gmail.com>
2022-04-28 22:34:39 +0200

    Add Player CRUD

---
+++ src/Form/PlayerType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Player;
+use App\Entity\Team;
+use App\Entity\Flag;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PlayerType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('playerName')
+            ->add('playerAvatar')
+            ->add('playerUplay')
+            ->add('playerAtTwitter')
+            ->add('playerDiscord')
+            ->add('playerTwitch')
+            ->add('playerStudentSA')
+            ->add('playerIDTeam', EntityType::class, ['class' => Team::class,
+            'choice_label' => 'team_name',
+            'multiple' => true,
+            'label' => 'Equipe'])
+            ->add('playerIDFlag', EntityType::class, ['class' => Flag::class,
+            'choice_label' => 'flag_name',
+            'label' => 'Pays'])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Player::class,
+        ]);
+    }
+}
