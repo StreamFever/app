6dadb8798cda8864f9ea86e361c401786d6bd254
Alexis BRIET <alexis.briet2003@gmail.com>
2022-05-05 19:06:20 +0200

    :zap: Update Entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,12 +3,16 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
  */
-class User
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     /**
      * @ORM\Id
@@ -18,43 +22,134 @@ class User
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=50, nullable=true)
+     * @ORM\Column(type="string", length=180, unique=true)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $roles = [];
+
+    /**
+     * @var string The hashed password
+     * @ORM\Column(type="string")
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $userFirstName;
 
     /**
-     * @ORM\Column(type="string", length=50, nullable=true)
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $userLastName;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\OneToMany(targetEntity=Overlay::class, mappedBy="widgetOwner")
      */
-    private $userPseudo;
+    private $overlayOwned;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\ManyToMany(targetEntity=Overlay::class, mappedBy="WidgetPermission")
      */
-    private $userEmail;
+    private $overlaysAllowed;
 
-    /**
-     * @ORM\Column(type="text")
-     */
-    private $userPassword;
-
-    /**
-     * @ORM\Column(type="text", nullable=true)
-     */
-    private $userToken;
-
-    /**
-     * @ORM\ManyToOne(targetEntity=Role::class, inversedBy="users")
-     */
-    private $userRole;
+    public function __construct()
+    {
+        $this->overlayOwned = new ArrayCollection();
+        $this->overlaysAllowed = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @deprecated since Symfony 5.3, use getUserIdentifier instead
+     */
+    public function getUsername(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Returning a salt is only needed, if you are not using a modern
+     * hashing algorithm (e.g. bcrypt or sodium) in your security.yaml.
+     *
+     * @see UserInterface
+     */
+    public function getSalt(): ?string
+    {
+        return null;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
     }
 
     public function getUserFirstName(): ?string
@@ -81,62 +176,59 @@ class User
         return $this;
     }
 
-    public function getUserPseudo(): ?string
+    /**
+     * @return Collection<int, Overlay>
+     */
+    public function getOverlayOwned(): Collection
     {
-        return $this->userPseudo;
+        return $this->overlayOwned;
     }
 
-    public function setUserPseudo(string $userPseudo): self
+    public function addOverlayOwned(Overlay $overlayOwned): self
     {
-        $this->userPseudo = $userPseudo;
+        if (!$this->overlayOwned->contains($overlayOwned)) {
+            $this->overlayOwned[] = $overlayOwned;
+            $overlayOwned->setWidgetOwner($this);
+        }
 
         return $this;
     }
 
-    public function getUserEmail(): ?string
+    public function removeOverlayOwned(Overlay $overlayOwned): self
     {
-        return $this->userEmail;
-    }
-
-    public function setUserEmail(string $userEmail): self
-    {
-        $this->userEmail = $userEmail;
+        if ($this->overlayOwned->removeElement($overlayOwned)) {
+            // set the owning side to null (unless already changed)
+            if ($overlayOwned->getWidgetOwner() === $this) {
+                $overlayOwned->setWidgetOwner(null);
+            }
+        }
 
         return $this;
     }
 
-    public function getUserPassword(): ?string
+    /**
+     * @return Collection<int, Overlay>
+     */
+    public function getOverlaysAllowed(): Collection
     {
-        return $this->userPassword;
+        return $this->overlaysAllowed;
     }
 
-    public function setUserPassword(string $userPassword): self
+    public function addOverlaysAllowed(Overlay $overlaysAllowed): self
     {
-        $this->userPassword = $userPassword;
+        if (!$this->overlaysAllowed->contains($overlaysAllowed)) {
+            $this->overlaysAllowed[] = $overlaysAllowed;
+            $overlaysAllowed->addWidgetPermission($this);
+        }
 
         return $this;
     }
 
-    public function getUserToken(): ?string
+    public function removeOverlaysAllowed(Overlay $overlaysAllowed): self
     {
-        return $this->userToken;
-    }
-
-    public function setUserToken(?string $userToken): self
-    {
-        $this->userToken = $userToken;
-
-        return $this;
-    }
-
-    public function getUserRole(): ?Role
-    {
-        return $this->userRole;
-    }
-
-    public function setUserRole(?Role $userRole): self
-    {
-        $this->userRole = $userRole;
+        if ($this->overlaysAllowed->removeElement($overlaysAllowed)) {
+            $overlaysAllowed->removeWidgetPermission($this);
+        }
 
         return $this;
     }
