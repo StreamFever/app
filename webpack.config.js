2ff0b2862cefb78a278a3d46f2d4ff619e37fb19
BRIETGAME <alexis.briet2003@gmail.com>
2022-06-06 19:50:21 +0200

    Config files

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,8 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/app.js')
+    .addEntry('app', './assets/apps/app.js')
+    .addEntry('app_overlay', './assets/apps/app_overlay.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
@@ -70,6 +71,6 @@ Encore
 
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
-;
+    ;
 
 module.exports = Encore.getWebpackConfig();
