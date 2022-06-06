ed502dcb19a3fa1552d50e7cf076bc8f8ef58ec5
BRIETGAME <alexis.briet2003@gmail.com>
2022-06-06 19:57:21 +0200

    Symfony app js files

--- assets/apps/app.js
+++ assets/apps/app.js
@@ -7,8 +7,11 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 // import './styles/app.css';
-import './styles/font.scss';
-import './styles/dashboard.scss';
+import '../styles/dashboard.scss';
+import '../styles/animations/index.scss';
 
 // start the Stimulus application
-import './bootstrap';
+import '../bootstrap';
+import '../js/admin/index';
+
+console.log("TEST");
