0d09ea7bf03489ee0d2dc0ccc87b51a95c090705
BRIETGAME <alexis.briet2003@gmail.com>
2022-06-15 19:27:14 +0200

    Update migrations

--- migrations/Version20220613190724.php
+++ migrations/Version20220613190724.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220608103425 extends AbstractMigration
+final class Version20220613190724 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20220608103425 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user ADD pseudo VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE meta_overlays CHANGE meta_value meta_value LONGTEXT DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user DROP pseudo');
+        $this->addSql('ALTER TABLE meta_overlays CHANGE meta_value meta_value LONGTEXT NOT NULL');
     }
 }
