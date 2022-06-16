9f18bd37fa352ba0d526f92ed275cfd5dcee65d2
BRIETGAME <alexis.briet2003@gmail.com>
2022-06-16 15:26:13 +0200

    Update WS Script

--- assets/js/admin/ws.js
+++ assets/js/admin/ws.js
@@ -1,9 +1,11 @@
 export const nameOfFunction = () => {
     const socket = io("https://websocketv2.artaic.fr");
 
-    socket.emit('auth', [document.getElementById('userID').value,document.getElementById('username').value])
-    
-	console.log('Authentification')
+    if (window.location.pathname.includes('/admin/overlay/u/')) { //Si client sur un panneau de contrôle, data[2] = ID pour recherche de room
+        socket.emit('auth', [document.getElementById('userID').value, document.getElementById('username').value, window.location.pathname.replace('/admin/overlay/u/', '')]);
+    } else { //Sinon authentification classique
+        socket.emit('auth', [document.getElementById('userID').value, document.getElementById('username').value])
+    }
 
     socket.on("disconnect", () => {
         location.reload()
