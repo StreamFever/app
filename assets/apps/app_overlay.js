8194191e0e4e5608103210d836f07c611fa3c9c0
Artaic-fr <jeremy.chaumet@gmail.com>
2022-06-29 19:04:56 +0200

    Update App Overlay

--- assets/apps/app_overlay.js
+++ assets/apps/app_overlay.js
@@ -9,10 +9,20 @@
 // import './styles/app.css';
 
 // TODO: Importer selon la version des widgets, soit le index des dossiers ou individuellement les styles
+import '../styles/lib/loader.scss';
 import '../styles/widgets/cc/index.scss';
 import '../js/admin/control_widget';
 import '../js/widgets/index';
+import { initWsServer } from '../js/admin/ws';
 
+initWsServer.call();
+
+$(function () { //equals window.onload or ready
+ $('#loading-wrapper').addClass('slide-out-fwd-center');
+ setTimeout(() => {
+  $('#loading-wrapper').hide()
+ }, 1000);
+});
 
 
 console.log("JS & CSS Overlay OK");
