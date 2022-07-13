2c0dea239cf9dacf8d35dfb44960085b4a214ff5
Artaic-fr <jeremy.chaumet@gmail.com>
2022-07-13 21:36:49 +0200

    Update entity

--- src/Entity/Overlay.php
+++ src/Entity/Overlay.php
@@ -40,15 +40,32 @@ class Overlay
      */
     private $widgets;
 
+    /**
+     * @ORM\OneToMany(targetEntity=Event::class, mappedBy="overlayId")
+     */
+    private $events;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Game::class, mappedBy="overlayId")
+     */
+    private $games;
+
     public function __construct()
     {
         $this->OverlayAccess = new ArrayCollection();
         $this->widgets = new ArrayCollection();
+        $this->events = new ArrayCollection();
+        $this->games = new ArrayCollection();
     }
 
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function __toString()
+    {
+        return $this->OverlayName;
     }
 
     public function getOverlayName(): ?string
@@ -123,6 +140,66 @@ class Overlay
             // set the owning side to null (unless already changed)
             if ($widget->getOverlay() === $this) {
                 $widget->setOverlay(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Event>
+     */
+    public function getEvents(): Collection
+    {
+        return $this->events;
+    }
+
+    public function addEvent(Event $event): self
+    {
+        if (!$this->events->contains($event)) {
+            $this->events[] = $event;
+            $event->setOverlayId($this);
+        }
+
+        return $this;
+    }
+
+    public function removeEvent(Event $event): self
+    {
+        if ($this->events->removeElement($event)) {
+            // set the owning side to null (unless already changed)
+            if ($event->getOverlayId() === $this) {
+                $event->setOverlayId(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Game>
+     */
+    public function getGames(): Collection
+    {
+        return $this->games;
+    }
+
+    public function addGame(Game $game): self
+    {
+        if (!$this->games->contains($game)) {
+            $this->games[] = $game;
+            $game->setOverlayId($this);
+        }
+
+        return $this;
+    }
+
+    public function removeGame(Game $game): self
+    {
+        if ($this->games->removeElement($game)) {
+            // set the owning side to null (unless already changed)
+            if ($game->getOverlayId() === $this) {
+                $game->setOverlayId(null);
             }
         }
 
