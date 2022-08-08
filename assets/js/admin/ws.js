8f090daff774a8a1540d573694eb6e45bce4eae4
BRIETGAME <alexis.briet2003@gmail.com>
2022-08-08 14:22:16 +0200

    Update WS call to isolate element

--- assets/js/admin/ws.js
+++ assets/js/admin/ws.js
@@ -3,14 +3,11 @@ export const socket = io("https://websocketv2.artaic.fr");
 export const initWsServer = () => {
     console.log(window.location.pathname)
     if (window.location.pathname.startsWith('/admin/overlay/')) { //Si client sur un panneau de contrôle, data[2] = ID pour recherche de room
-        socket.emit('auth', [document.getElementById('userID').value, document.getElementById('username').value, window.location.pathname, window.location.pathname.replace('/admin/overlay/', '')], () => { console.log("Hello") });
-        console.log('auth from panel')
+        socket.emit('auth', [document.getElementById('userID').value, document.getElementById('username').value, window.location.pathname, window.location.pathname.replace('/admin/overlay/', '').split('/')[0]], () => { console.log("Hello") });
     } else if (window.location.pathname.startsWith('/overlay/')) { //Connexion sur un overlay
         socket.emit('auth', [window.location.pathname.replace('/overlay/', '').split('/')[0], "Browsersource", window.location.pathname, window.location.pathname.replace('/overlay/', '').split('/')[0]], () => { console.log("Hello") });
-        console.log('auth from overlay')
     } else { //Sinon authentification classique
         socket.emit('auth', [document.getElementById('userID').value, document.getElementById('username').value, window.location.pathname], () => { console.log("Hello") })
-        console.log('auth')
     }
 
     socket.on("disconnect", () => {
@@ -29,3 +26,6 @@ export const initWsServer = () => {
         window.location.replace("https://v4.dev.symfony.artaic.fr")
     })
 }
+
+
+initWsServer();
