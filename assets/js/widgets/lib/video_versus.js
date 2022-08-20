496269b5a4ec7f935faf054bdcfe793aab5a8b7f
BRIETGAME <alexis.briet2003@gmail.com>
2022-08-20 22:56:45 +0200

    update ShowVideoVersus

--- assets/js/widgets/lib/video_versus.js
+++ assets/js/widgets/lib/video_versus.js
@@ -2,30 +2,17 @@ import { socket } from "../../admin/ws";
 
 let versusVisible = false;
 
-export const showVideoVersus = (name_map) => {
+
+export const showVideoVersus = () => {
+    let name_map = document.getElementById('video_versus').dataset.map;
     if (name_map != false) {
         versusVisible = true;
-        if (topbar.className != 'display_none') {
-            socket.emit("hide_topbar");
-        }
-        if (leftpopuptext.className != 'display_none' && textpopuptext.className != 'display_none') {
-            socket.emit("hide_popup");
-        }
-        if (teams_nextmatch.className != 'display_none' && textnextmatch.className != 'display_none') {
-            socket.emit("hide_nextmatch");
-        }
-        // if (lsd_cam.className != 'display_none') {
-        //     socket.emit("hide_camlsd");
-        // }
-        if (bottombar.className != 'display_none') {
-            socket.emit("hide_bottombar");
-        }
         setTimeout(() => {
-            video_versus.innerHTML = ` <video muted id="video_element_versus" width="1920" height="1080" autoplay onended="offVersus()">
-            <source src="https://cdn.artaic.fr/videos/maps/${name_map}_v2.mp4" type="video/mp4">
+            video_versus.innerHTML = ` <video muted id="video_element_versus" width="1920" height="1080" autoplay onended="offVersus(); changeAuto('video_versus')">
+            <source src="https://cdn.artaic.fr/stream_cave/videos/maps/${name_map}_v2.mp4" type="video/mp4">
         </video> 
         <audio autoplay id="audio_element_versus">
-            <source src="https://cdn.artaic.fr/videos/maps/${name_map}_v2.mp3" type="audio/mpeg">
+            <source src="https://cdn.artaic.fr/stream_cave/videos/maps/${name_map}_v2.mp3" type="audio/mpeg">
         </audio> 
         `;
             video_versus.className = "fade-in";
