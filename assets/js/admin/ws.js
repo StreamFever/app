f4483cc1dd7e16345a68b462fe45d32170966f98
BRIETGAME <alexis.briet2003@gmail.com>
2022-08-20 22:55:58 +0200

    update ws file client

--- assets/js/admin/ws.js
+++ assets/js/admin/ws.js
@@ -2,7 +2,9 @@ export const socket = io("https://websocketv2.artaic.fr");
 
 export const initWsServer = () => {
     console.log(window.location.pathname)
-    if (window.location.pathname.startsWith('/admin/overlay/')) { //Si client sur un panneau de contrôle, data[2] = ID pour recherche de room
+    if(window.location.pathname == "/admin/overlay/new"){//Si le client essaye de créer un nouvel overlay
+        socket.emit('auth', [document.getElementById('userID').value, document.getElementById('username').value, window.location.pathname], () => { console.log("Hello") })
+    }else if (window.location.pathname.startsWith('/admin/overlay/')) { //Si client sur un panneau de contrôle, data[2] = ID pour recherche de room
         socket.emit('auth', [document.getElementById('userID').value, document.getElementById('username').value, window.location.pathname, window.location.pathname.replace('/admin/overlay/', '').split('/')[0]], () => { console.log("Hello") });
     } else if (window.location.pathname.startsWith('/overlay/')) { //Connexion sur un overlay
         socket.emit('auth', [window.location.pathname.replace('/overlay/', '').split('/')[0], "Browsersource", window.location.pathname, window.location.pathname.replace('/overlay/', '').split('/')[0]], () => { console.log("Hello") });
