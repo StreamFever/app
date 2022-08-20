9679ccaa3edf14f3db0c46678a8b09e62f3ca426
BRIETGAME <alexis.briet2003@gmail.com>
2022-08-20 23:17:49 +0200

    update eventEdition

--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -4,10 +4,12 @@ namespace App\Form;
 
 use App\Entity\Event;
 use App\Entity\Edition;
+use App\Entity\Game;
 use App\Entity\Sponsor;
 use App\Entity\Social;
 
 use App\Entity\Overlay;
+use App\Repository\GameRepository;
 use App\Repository\OverlayRepository;
 
 use Symfony\Component\Security\Core\Security;
@@ -24,7 +26,8 @@ use Symfony\Component\Validator\Constraints\File;
 class EventType extends AbstractType
 {
 
-    public function __construct(Security $security) {
+    public function __construct(Security $security)
+    {
         $this->security = $security;
     }
 
@@ -35,14 +38,16 @@ class EventType extends AbstractType
                 'attr' => array(
                     'placeholder' => 'hereYourPlaceHolder'
                 )
-           ))
-           ->add('eventEdition', EntityType::class, ['class' => Edition::class,
-           'choice_label' => 'edition_name',
-           'label' => 'Edition'],  null, array(
-               'attr' => array(
-                   'placeholder' => 'hereYourPlaceHolder'
-               )
-          ))
+            ))
+            ->add('eventEdition', EntityType::class, [
+                'class' => Edition::class,
+                'choice_label' => 'edition_name',
+                'label' => 'Edition'
+            ],  null, array(
+                'attr' => array(
+                    'placeholder' => 'hereYourPlaceHolder'
+                )
+            ))
             ->add('eventLogo', FileType::class, [
                 'label' => 'Logo event',
 
@@ -70,12 +75,12 @@ class EventType extends AbstractType
                 'attr' => array(
                     'placeholder' => 'hereYourPlaceHolder'
                 )
-           ))
+            ))
             ->add('eventCashprize',  null, array(
                 'attr' => array(
                     'placeholder' => 'hereYourPlaceHolder'
                 )
-           ))
+            ))
             ->add('eventStartDate', DateTimeType::class, [
                 'widget' => 'single_text',
                 'placeholder' => [
@@ -90,48 +95,61 @@ class EventType extends AbstractType
                     'hour' => 'Hour', 'minute' => 'Minute', 'second' => 'Second',
                 ],
             ])
-            ->add('eventIdSponsor', EntityType::class, ['class' => Sponsor::class,
-            'choice_label' => 'sponsor_name',
-            'multiple' => true,
-            'label' => 'Sponsor(s)'],  null, array(
+            ->add('eventIdSponsor', EntityType::class, [
+                'class' => Sponsor::class,
+                'choice_label' => 'sponsor_name',
+                'multiple' => true,
+                'label' => 'Sponsor(s)'
+            ],  null, array(
                 'attr' => array(
                     'placeholder' => 'hereYourPlaceHolder'
                 )
-           ))
-           ->add('eventSocial', EntityType::class, ['class' => Social::class,
-            // 'choice_label' => 'social_lib_id',
-            'multiple' => true,
-            'label' => 'Réseaux sociaux'],  null, array(
+            ))
+            //    ->add('eventSocial', EntityType::class, ['class' => Social::class,
+            //     // 'choice_label' => 'social_lib_id',
+            //     'multiple' => true,
+            //     'label' => 'Réseaux sociaux'],  null, array(
+            //         'attr' => array(
+            //             'placeholder' => 'hereYourPlaceHolder'
+            //         )
+            //    ))
+            ->add('eventSlots',  null, array(
                 'attr' => array(
                     'placeholder' => 'hereYourPlaceHolder'
                 )
-           ))
-           ->add('eventSlots',  null, array(
-            'attr' => array(
-                'placeholder' => 'hereYourPlaceHolder'
+            ))
+            ->add('userId')
+            ->add('eventAccess')
+            ->add('eventFormat',  null, array(
+                'attr' => array(
+                    'placeholder' => 'hereYourPlaceHolder'
+                )
+            ))
+            ->add(
+                'overlayId',
+                EntityType::class,
+                [
+                    'class' => Overlay::class,
+                    'choice_label' => 'overlay_name',
+                    'query_builder' => function (OverlayRepository $overlayrepository) {
+                        return $overlayrepository->createQueryBuilder('o')
+                            ->join('o.OverlayAccess', 'u1')
+                            ->join('o.OverlayOwner', 'u2')
+                            ->where('u1.id = :user_id OR u2.id = :user_id')
+                            ->setParameter('user_id', $this->security->getUser()->getId())
+                            ->orderBy('o.id', 'ASC');
+                    },
+                ]
             )
-       ))
-           ->add('userId')
-           ->add('eventAccess')
-           ->add('eventFormat',  null, array(
-            'attr' => array(
-                'placeholder' => 'hereYourPlaceHolder'
+            ->add(
+                'currentGame',
+                EntityType::class,
+                [
+                    'class' => Game::class,
+                    'choice_label' => 'game_name',
+                ]
             )
-       ))
-       ->add('overlayId', EntityType::class, 
-       [
-           'class' => Overlay::class, 
-           'choice_label' => 'overlay_name',
-           'query_builder' => function (OverlayRepository $overlayrepository) {
-               return $overlayrepository->createQueryBuilder('o')
-               ->join('o.OverlayAccess', 'u1')
-               ->join('o.OverlayOwner', 'u2')
-               ->where('u1.id = :user_id OR u2.id = :user_id')
-               ->setParameter('user_id', $this->security->getUser()->getId())
-               ->orderBy('o.id', 'ASC');
-           },
-           ])
-        ;
+            ->add('socials');
     }
 
     public function configureOptions(OptionsResolver $resolver): void
