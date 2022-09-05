7986a9ed017be50fe2b8eab39b6ef98182e77b50
Artaic-fr <jeremy.chaumet@gmail.com>
2022-09-05 19:42:11 +0200

    Fix add properties

--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -3,29 +3,20 @@
 namespace App\Form;
 
 use App\Entity\Event;
-use App\Entity\Edition;
 use App\Entity\Game;
-use App\Entity\Sponsor;
-use App\Entity\Social;
-
 use App\Entity\Overlay;
-use App\Repository\GameRepository;
 use App\Repository\OverlayRepository;
-
-use Symfony\Component\Security\Core\Security;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
-use Symfony\Component\Form\Extension\Core\Type\HiddenType;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 
 class EventType extends AbstractType
 {
-
     public function __construct(Security $security)
     {
         $this->security = $security;
@@ -34,20 +25,8 @@ class EventType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('eventName',  null, array(
-                'attr' => array(
-                    'placeholder' => 'hereYourPlaceHolder'
-                )
-            ))
-            ->add('eventEdition', EntityType::class, [
-                'class' => Edition::class,
-                'choice_label' => 'edition_name',
-                'label' => 'Edition'
-            ],  null, array(
-                'attr' => array(
-                    'placeholder' => 'hereYourPlaceHolder'
-                )
-            ))
+            ->add('eventName')
+            ->add('eventHashtag')
             ->add('eventLogo', FileType::class, [
                 'label' => 'Logo event',
 
@@ -71,16 +50,8 @@ class EventType extends AbstractType
                     ])
                 ],
             ])
-            ->add('eventHashtag',  null, array(
-                'attr' => array(
-                    'placeholder' => 'hereYourPlaceHolder'
-                )
-            ))
-            ->add('eventCashprize',  null, array(
-                'attr' => array(
-                    'placeholder' => 'hereYourPlaceHolder'
-                )
-            ))
+            ->add('eventSlots')
+            ->add('eventCashprize')
             ->add('eventStartDate', DateTimeType::class, [
                 'widget' => 'single_text',
                 'placeholder' => [
@@ -95,36 +66,10 @@ class EventType extends AbstractType
                     'hour' => 'Hour', 'minute' => 'Minute', 'second' => 'Second',
                 ],
             ])
-            ->add('eventIdSponsor', EntityType::class, [
-                'class' => Sponsor::class,
-                'choice_label' => 'sponsor_name',
-                'multiple' => true,
-                'label' => 'Sponsor(s)'
-            ],  null, array(
-                'attr' => array(
-                    'placeholder' => 'hereYourPlaceHolder'
-                )
-            ))
-            //    ->add('eventSocial', EntityType::class, ['class' => Social::class,
-            //     // 'choice_label' => 'social_lib_id',
-            //     'multiple' => true,
-            //     'label' => 'Réseaux sociaux'],  null, array(
-            //         'attr' => array(
-            //             'placeholder' => 'hereYourPlaceHolder'
-            //         )
-            //    ))
-            ->add('eventSlots',  null, array(
-                'attr' => array(
-                    'placeholder' => 'hereYourPlaceHolder'
-                )
-            ))
+            ->add('eventEdition')
+            ->add('eventIdSponsor')
             ->add('userId')
-            ->add('eventAccess')
-            ->add('eventFormat',  null, array(
-                'attr' => array(
-                    'placeholder' => 'hereYourPlaceHolder'
-                )
-            ))
+            ->add('eventFormat')
             ->add(
                 'overlayId',
                 EntityType::class,
@@ -133,14 +78,15 @@ class EventType extends AbstractType
                     'choice_label' => 'overlay_name',
                     'query_builder' => function (OverlayRepository $overlayrepository) {
                         return $overlayrepository->createQueryBuilder('o')
-                            ->join('o.OverlayAccess', 'u1')
-                            ->join('o.OverlayOwner', 'u2')
-                            ->where('u1.id = :user_id OR u2.id = :user_id')
-                            ->setParameter('user_id', $this->security->getUser()->getId())
-                            ->orderBy('o.id', 'ASC');
+                            ->leftJoin('o.OverlayAccess', 'u1')
+                            ->where('u1 = :id_user OR o.OverlayOwner = :id_user')
+                            ->setParameter('id_user', $this->security->getUser()->getId())
+                            ->orderBy('o.OverlayName', 'ASC');
                     },
                 ]
             )
+            ->add('eventAccess')
+            ->add('socials')
             ->add(
                 'currentGame',
                 EntityType::class,
@@ -148,8 +94,7 @@ class EventType extends AbstractType
                     'class' => Game::class,
                     'choice_label' => 'game_name',
                 ]
-            )
-            ->add('socials');
+            );
     }
 
     public function configureOptions(OptionsResolver $resolver): void
