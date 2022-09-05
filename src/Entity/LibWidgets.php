88c95a87a1885ebdce9b0acaefaa382da98466ab
Artaic-fr <jeremy.chaumet@gmail.com>
2022-09-05 19:40:09 +0200

    Add widgets property

--- src/Entity/LibWidgets.php
+++ src/Entity/LibWidgets.php
@@ -2,8 +2,10 @@
 
 namespace App\Entity;
 
-use App\Repository\LibWidgetsRepository;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\LibWidgetsRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 
 /**
  * @ORM\Entity(repositoryClass=LibWidgetsRepository::class)
@@ -31,6 +33,16 @@ class LibWidgets
      * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $libWidgetId2;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Widgets::class, mappedBy="WidgetId")
+     */
+    private $widgets;
+
+    public function __construct()
+    {
+        $this->widgets = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -74,6 +86,36 @@ class LibWidgets
     public function setLibWidgetId2(?string $libWidgetId2): self
     {
         $this->libWidgetId2 = $libWidgetId2;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Widgets>
+     */
+    public function getWidgets(): Collection
+    {
+        return $this->widgets;
+    }
+
+    public function addWidget(Widgets $widget): self
+    {
+        if (!$this->widgets->contains($widget)) {
+            $this->widgets[] = $widget;
+            $widget->setWidgetId($this);
+        }
+
+        return $this;
+    }
+
+    public function removeWidget(Widgets $widget): self
+    {
+        if ($this->widgets->removeElement($widget)) {
+            // set the owning side to null (unless already changed)
+            if ($widget->getWidgetId() === $this) {
+                $widget->setWidgetId(null);
+            }
+        }
 
         return $this;
     }
