87aecbe2f2e627464dd99878dda8159a9a97a04c
Artaic-fr <jeremy.chaumet@gmail.com>
2022-09-05 19:44:49 +0200

    Update search function call

--- src/Service/Events.php
+++ src/Service/Events.php
@@ -9,26 +9,26 @@ use Doctrine\ORM\Mapping as ORM;
 class Events
 {
 
- public function __construct(Security $security, EventRepository $eventRepository)
- {
-     // Avoid calling getUser() in the constructor: auth may not
-     // be complete yet. Instead, store the entire Security object.
-     $this->security = $security;
-     $this->EventRepository = $eventRepository;
- }
+    public function __construct(Security $security, EventRepository $eventRepository)
+    {
+        // Avoid calling getUser() in the constructor: auth may not
+        // be complete yet. Instead, store the entire Security object.
+        $this->security = $security;
+        $this->EventRepository = $eventRepository;
+    }
 
- public function getAllEvents(): Array
- {
-   return $this->EventRepository->findAll();
- }
+    public function getAllEvents(): array
+    {
+        return $this->EventRepository->findAll();
+    }
 
- public function getOneCurrentEvent(): Array
- {
-   return $this->EventRepository->findFirst();
- }
+    public function getOneCurrentEvent(): array
+    {
+        return $this->EventRepository->findFirst();
+    }
 
- public function getRecentsEvent(): Array
- {
-   return $this->EventRepository->findRecentsEvent();
- }
+    public function getRecentsEvent(): array
+    {
+        return $this->EventRepository->findRecentsEvent();
+    }
 }
