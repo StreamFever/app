6ddc26ff44f8b4d20f1e464bee8954de03348985
Artaic-fr <jeremy.chaumet@gmail.com>
2022-09-13 21:38:55 +0200

    Reset query builder widgetId

--- src/Form/WidgetsType.php
+++ src/Form/WidgetsType.php
@@ -57,15 +57,6 @@ class WidgetsType extends AbstractType
                 [
                     'class' => LibWidgets::class,
                     'choice_label' => 'libWidgetName',
-                    'query_builder' => function (LibWidgetsRepository $librepository) {
-                        return $librepository->createQueryBuilder('l')
-                            ->leftJoin('l.widgets', 'w1')
-                            ->leftJoin('w1.overlay', 'o')
-                            ->leftJoin('o.widgets', 'w2')
-                            ->leftJoin('w2.WidgetId', 'l2')
-                            ->where('o.id = :id_overlay AND l2.id != l.id')
-                            ->setParameter('id_overlay', 1);
-                    },
                 ]
             )
             ->add(
@@ -83,39 +74,7 @@ class WidgetsType extends AbstractType
                     },
                 ]
             )
-            // INFO: Lors de l'édition d'un widget, afficher "isTwoWidgets" si celui-ci est un widget de "deux widgets"
-            ->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
-                $form = $event->getForm();
-                $data = $event->getData();
-                // dd($data);
-                if (!empty($data->getWidgetId())) {
-                    if ($data->getWidgetId()->getLibWidgetId2() != null) {
-                        $form->add('isTwoWidgets');
-                    } else {
-                        $form->setData(['isTwoWidgets' => 0]);
-                    }
-                }
-
-                $finalData = [];
-                $allLibWidget = $this->LibWidgetsRepository->findAll();
-                $libWidgetIntoOverlay = $this->LibWidgetsRepository->findAllByOverlay(2);
-
-                dump($allLibWidget);
-                dump($libWidgetIntoOverlay);
-                // INFO: Loop dans la liste des LibWidgets DE L'OVERLAY
-                for ($i1 = 0; $i1 < count($libWidgetIntoOverlay); $i1++) {
-                    // INFO: Loop dans la liste des LibWidgets DE L'OVERLAY
-                    for ($i2 = 0; $i2 < count($allLibWidget); $i2++) {
-                        dump($libWidgetIntoOverlay[$i1]->getLibWidgetId() . ' ' . $allLibWidget[$i2]->getLibWidgetId());
-
-                        if ($libWidgetIntoOverlay[$i1]->getLibWidgetId() != $allLibWidget[$i2]->getLibWidgetId()) {
-                            $finalData[$i1] = $libWidgetIntoOverlay[$i1];
-                        }
-                    }
-                }
-                dd($finalData);
-            });
-
+            ->add('isTwoWidgets');
         // INFO: Lors de la création d'un widget, afficher "isTwoWidgets" si celui-ci est un widget de "deux widgets"
         // $builder->addEventListener(
         //     FormEvents::PRE_SUBMIT,
