18cb271d7c10faacdf782fe68673c8d89ec782ec
Artaic-fr <jeremy.chaumet@gmail.com>
2022-09-16 22:30:47 +0200

    Add security dom

--- assets/js/widgets/topbar.js
+++ assets/js/widgets/topbar.js
@@ -1,6 +1,10 @@
 console.log("TOP BAR");
-var myCarousel = document.querySelector('#myCarousel')
-var carousel = new bootstrap.Carousel(myCarousel, {
- interval: 10000,
- wrap: false
-})
+console.log(document.querySelector('#myCarousel'));
+
+if (document.querySelector('#myCarousel') != null) {
+ var myCarousel = document.querySelector('#myCarousel')
+ var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: 10000,
+  wrap: false
+ })
+}
