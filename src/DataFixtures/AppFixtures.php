cb8e26f2a9c10c289b621b57f28d50fbd6239c73
Artaic-fr <jeremy.chaumet@gmail.com>
2022-09-17 09:00:28 +0200

    Add new user

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -18,7 +18,7 @@ class AppFixtures extends Fixture
         $this->hasher = $hasher;
     }
 
-    public function setBrietgame($manager): void 
+    public function setBrietgame($manager): void
     {
         $user = new User();
         $user->setEmail('alexis.briet2003@gmail.com');
@@ -36,7 +36,7 @@ class AppFixtures extends Fixture
         $manager->persist($user);
     }
 
-    public function setArtaic($manager): void 
+    public function setArtaic($manager): void
     {
         $user = new User();
         $user->setEmail('chajer@live.com');
@@ -54,7 +54,7 @@ class AppFixtures extends Fixture
         $manager->persist($user);
     }
 
-    public function setWeboscket($manager): void 
+    public function setWeboscket($manager): void
     {
         $user = new User();
         $user->setEmail('websocket@artaic.fr');
@@ -63,6 +63,70 @@ class AppFixtures extends Fixture
         $user->setRoles(['ROLE_ADMIN']);
 
         $password = $this->hasher->hashPassword($user, 'test');
+        $user->setPassword($password);
+
+        $this->addReference('user_3', $user);
+
+        $manager->persist($user);
+    }
+
+    public function setOlaf($manager): void
+    {
+        $user = new User();
+        $user->setEmail('steven.derain4@gmail.com');
+        $user->setPseudo('Olaf');
+        $user->setUuid(Uuid::uuid4());
+        $user->setRoles(['ROLE_ADMIN']);
+
+        $password = $this->hasher->hashPassword($user, 'T6DNrXMEfPM4Ha7z');
+        $user->setPassword($password);
+
+        $this->addReference('user_3', $user);
+
+        $manager->persist($user);
+    }
+
+    public function setJulien($manager): void
+    {
+        $user = new User();
+        $user->setEmail('zuulienn@gmail.com');
+        $user->setPseudo('Julien');
+        $user->setUuid(Uuid::uuid4());
+        $user->setRoles(['ROLE_ADMIN']);
+
+        $password = $this->hasher->hashPassword($user, '5KMHA5DzX7kd5isR');
+        $user->setPassword($password);
+
+        $this->addReference('user_3', $user);
+
+        $manager->persist($user);
+    }
+
+    public function setAcoui($manager): void
+    {
+        $user = new User();
+        $user->setEmail('axeloudart67130@gmail.com');
+        $user->setPseudo('Julien');
+        $user->setUuid(Uuid::uuid4());
+        $user->setRoles(['ROLE_ADMIN']);
+
+        $password = $this->hasher->hashPassword($user, '89coDHjdg9FsMEF6');
+        $user->setPassword($password);
+
+        $this->addReference('user_3', $user);
+
+        $manager->persist($user);
+    }
+
+    public function setExcel($manager): void
+    {
+        $user = new User();
+        $user->setEmail('simon.lecoulant35450@gmail.com');
+        $user->setPseudo('Excel');
+        $user->setUuid(Uuid::uuid4());
+        $user->setRoles(['ROLE_ADMIN']);
+
+        $password = $this->hasher->hashPassword($user, 'aF5RrCdH7rJydHiE');
         $user->setPassword($password);
 
         $this->addReference('user_3', $user);
